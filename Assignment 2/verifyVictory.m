78a8322c9ce6ed5c48c007fb6e1bd80e87f26e02
simonesinceri <56643807+simonesinceri@users.noreply.github.com>
2021-10-23 00:23:20 +0200

    add verifyVictory.m

---
+++ Assignment 2/verifyVictory.m
@@ -0,0 +1,8 @@
+% funzione che esamina lo stato e determina
+% se è finale, assegnando la reward in base al vincitore
+function verifyVictory(state)
+
+% problema nel caso 2 sequenze di vittoria
+% me la spiccio con l'ordine degli if
+
+end
