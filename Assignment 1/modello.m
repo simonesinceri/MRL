abd894c4baebb546869b6904c374953597ac4ebb
simonesinceri <56643807+simonesinceri@users.noreply.github.com>
2022-03-18 11:06:57 +0100

    modifica modello stocastico dei Bandit

--- Assignment 1/modello.m
+++ Assignment 1/modello.m
@@ -21,7 +21,7 @@ for i=1:T
     R(i,:) = normrnd(mu(i), sigma,1,10);
    % mu(i+1,:) = randi([-1 1],1,10); % ????
     %mu(i+1,:) = rand(1,10)-rand(1,10);
-    mu(i+1,:) = mu(i,:) + 0.001*normrnd(0,10,1,10);
+    mu(i+1,:) = mu(i,:) + 0.001*normrnd(0,10,1,10); % prova 0.1
     
 end
 
